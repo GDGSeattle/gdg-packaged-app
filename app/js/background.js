ab0ca2be67021f779e29ba4e902f48846c24042e
Mike Koss <mckoss@startpad.org>
2013-07-08 23:55:48 -0700

    error format

--- app/js/background.js
+++ app/js/background.js
@@ -1,7 +1,3 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('index.html',
-                             {
-                                 bounds: {width: 700, height: 500}
-                                 // frame: 'none'
-                             });
+    chrome.app.window.create('index.html', {bounds: {width: 700, height: 500}});
 });
