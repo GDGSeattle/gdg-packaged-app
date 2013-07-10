0362cb8f586649ea59fc230cd935f771ed6afc0b
Mike Koss <mckoss@startpad.org>
2013-07-10 10:45:28 -0700

    factor controllers to separate files (but retain same module)

---
+++ app/js/about.js
@@ -0,0 +1,9 @@
+angular.module('gdgPackagedApp')
+    .controller({
+        AboutController: function($scope) {
+            console.log("AboutController");
+            namespace.gdg.network.init(function () {
+                $scope.adaptors = namespace.gdg.network.getAdaptors();
+            });
+        }
+    });
---
+++ app/js/chat.js
@@ -0,0 +1,53 @@
+angular.module('gdgPackagedApp')
+    .controller({
+        ChatController: function($scope, $rootScope, $timeout, MessageService) {
+            $scope.$watch('userName', function() {
+                $rootScope.userName = $scope.userName;
+            });
+
+            // Set the input focus appropriately when the view is created.
+            if (!$scope.userName) {
+                $scope.focusName = true;
+            } else {
+                $scope.focusMessage = true;
+            }
+
+            // TODO: not angular-idiomatic - should use a directive?
+            var chatDiv = $('div.chat')[0];
+
+            $scope.sendMessage = function () {
+                $scope.showError('');
+                if (!$scope.userName) {
+                    $scope.showError("Please enter a user name.");
+                    $scope.focusName = true;
+                    return;
+                }
+                $scope.focusName = false;
+
+                if (!$scope.messageText) {
+                    $scope.showError("Empty message.");
+                    return;
+                }
+
+                MessageService.addMessage({from: $scope.userName,
+                                           text: $scope.messageText});
+                $scope.messageText = "";
+            }
+
+            MessageService.addListener(function(messages) {
+                // Need call $apply since this is an aysnc update to the scope.
+                $scope.$apply(function () {
+                    $scope.udpServer = MessageService.udpServer;
+                    if (!messages) {
+                        return;
+                    }
+                    $scope.messages = messages;
+                    // After page update - scroll to bottom - better to force scroll to bottom
+                    // on update?
+                    $timeout(function() {
+                        chatDiv.scrollTop = chatDiv.scrollHeight;
+                    });
+                });
+            });
+        }
+    });
